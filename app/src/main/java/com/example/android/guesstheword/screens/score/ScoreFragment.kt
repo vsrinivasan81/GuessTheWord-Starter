5e0ffee4db4b6a2b756064c1db5c61a593cebd00
Vijay Srinivasan <cbevijay@gmail.com>
2020-05-14 19:28:48 +0530

    Added missing code for score fragment class

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
@@ -45,6 +46,14 @@ class ScoreFragment : Fragment() {
                 false
         )
 
+        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+        binding.scoreText.text = scoreFragmentArgs.score.toString()
+        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
         return binding.root
+    }
+
+    private fun onPlayAgain() {
+        findNavController().navigate(ScoreFragmentDirections.actionRestart())
     }
 }
